037be54a243529f57b605d9b4fb015733218cce5
pengyu-wang <60125120+pengyu-wang@users.noreply.github.com>
2020-01-22 23:57:17 -0600

    I choose a wrong file.... no.2
    this time should me ok

--- CodingBatCodingExercises.java
+++ CodingBatCodingExercises.java
@@ -10,6 +10,18 @@ public int factorial(int n) {
   
   return n * factorial ( n - 1 );
   else return n;
-  
+
 }
+
+//We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+//bunnyEars(0) -> 0
+//bunnyEars(1) -> 2
+//bunnyEars(2) -> 4
+
+public int bunnyEars(int bunnies) {
+  if (bunnies >= 1)
+  return (2 + (bunnyEars ( bunnies - 1 )));
+  else return bunnies;
+}
+
 
